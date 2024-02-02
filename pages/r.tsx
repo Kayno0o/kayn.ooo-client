df155d7e24df56d722d8279498d98946d5eeed34
Kevyn Fyleyssant <kevyn.fyleyssant@gmail.com>
2024-02-03 00:17:10 +0100

    Update Meta component in r.tsx

--- pages/r.tsx
+++ pages/r.tsx
@@ -10,7 +10,11 @@ function PageR() {
   })
 
   return <>
-    <Meta title="Ma base Enshrouded !" src='https://cdn.discordapp.com/attachments/828388340466057247/1202358881762951269/image.png?ex=65cd2af8&is=65bab5f8&hm=4cc833e212eb7545d26333ed4feb7be8c12340f0aed4570e2567ce3f525d3434&' />
+    <Meta
+      title="Ma base Enshrouded !"
+      src='https://cdn.discordapp.com/attachments/828388340466057247/1202358881762951269/image.png?ex=65cd2af8&is=65bab5f8&hm=4cc833e212eb7545d26333ed4feb7be8c12340f0aed4570e2567ce3f525d3434&'
+      description='Petit tour en vidéo de ma magnifique base sur le jeu Enshrouded, en coopération avec mon ami Fauchette qui a fait toute la déco intérieure et extérieure !'
+    />
   </>
 }
 
